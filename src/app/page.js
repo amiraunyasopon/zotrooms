3e4746db1f9cb747812fccb4cd53828f4c9f22c7
isak <isakmpederson@gmail.com>
2025-11-08 17:34:32 -0800

    Import navbar and add few useState functionalities.

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,24 @@ import Navbar from "../components/Navbar"
 import { useState, useEffect } from 'react';
 import { fetchRooms, fetchRoom } from "./api/user-api";
 
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import * as React from 'react';
+
+
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
 export default function MainPage() {
     const [data, setData] = useState([]);
     const [buildings, setBuildings] = useState({ LangsonLibrary: true, ScienceLibrary: true, GatewayStudyCenter: true, MultimediaResourcesCenter: true, GrunigenMedicalLibrary: true });
     const [minCapacity, setMinCapacity] = useState(1000);
     const [startTime, setStartTime] = useState(0);
     const [endTime, setEndTime] = useState(0);
-    const [day, setDay] = useState(0);
+    const [day, setDay] = useState(null);
     // all buttons for buildings initially are selected
     // if a button is selected it turns the value for the building true
     // if unselected it turns the value for the building false
@@ -40,10 +51,78 @@ export default function MainPage() {
         console.log(data.filter((room) => room.location === "Science Library"))
     }, [])
 
+    useEffect(() => {
+       console.log(day + " " + startTime + " " + endTime + "library science " + buildings.ScienceLibrary)
+   }, [day, startTime, endTime, buildings]);
+
+
+  
+       const handleLocationChange = (e, type) => {
+        
+        let newVal = e.target.checked
+        console.log(newVal + " " + type)
+        setBuildings((old) => ({
+            ...old,
+            type: newVal
+        }))
+    
+    console.log(buildings.type + "  asdfasdf")
+console.log(day + " " + startTime + " " + endTime + "library science " + buildings.ScienceLibrary)}
+
+
+
     return (
         <>
             <div className="min-h-screen w-screen">
-                <Navbar />
+                
+                
+
+
+                <div className="bg-[#255799] w-1/4 h-full  relative overflow-auto">
+               <div className="flex flex-row items-center">
+                   <img src="logo.png"  className=" h-2/3 w-2/3 "></img>
+               <h1 className="text-[1.90vw] font-serif text-white font-extrabold overflow-hidden w-3/4" >Zot Rooms</h1></div>
+           <h2 className="text-[1.10vw] font-serif text-white font-bold overflow-hidden w-3/4 w-full text-center" >Select Date</h2>
+      
+      
+               <LocalizationProvider dateAdapter={AdapterDayjs}>
+           
+                 <DateCalendar value={day} onChange={(newDay) => setDay(newDay)} className="text-white" />
+             
+           </LocalizationProvider>
+      
+      
+      
+      
+           <h2 className="text-[1.10vw] font-serif text-white font-bold overflow-hidden w-3/4 w-full text-center" >Select Time</h2>
+           <div className="w-full flex flex-row justify-center gap-7"> <input
+               type="time"
+               id="startTime" onChange={(e) => setStartTime(e.target.value)}/>
+      
+      
+           <input
+               type="time"
+               id="endTime" onChange={(e) => setEndTime(e.target.value)}/> </div>
+      
+      
+           <h2 className="text-[1.10vw] font-serif text-white font-bold overflow-hidden w-3/4 w-full text-center mt-5" >Select Location</h2>
+         
+      
+      
+           <FormGroup className='w-full  flex flex-col items-center'>
+           <FormControlLabel control={<Checkbox defaultChecked />} label="Science Library" onChange={(e) => handleLocationChange(e, "ScienceLibrary")} className='w-2/3 flex justify-start'/>
+           <FormControlLabel control={<Checkbox defaultChecked />} label="Langson Library"  className='w-2/3 flex justify-start'/>
+           <FormControlLabel control={<Checkbox defaultChecked />} label="Gateway Study Center" className='w-2/3 flex justify-start'/>
+           <FormControlLabel control={<Checkbox defaultChecked />} label="Multimedia Resources Center" className='w-2/3 flex justify-start'/>
+           <FormControlLabel control={<Checkbox defaultChecked />} label="Grunigen Medical Library" className='w-2/3 flex justify-start'/>
+           </FormGroup>
+         
+         
+      
+      
+           </div>
+
+
                 <div>
                     <h1>Science Library</h1>
                     {
